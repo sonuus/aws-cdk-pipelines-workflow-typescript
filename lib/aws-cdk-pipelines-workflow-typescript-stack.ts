bb12bc48b3fd5b0499f309e59bf13c1e3c49c663
Sachin Doshi <sddoshi@amazon.com>
2021-09-06 18:42:17 -0400

    ...

--- lib/aws-cdk-pipelines-workflow-typescript-stack.ts
+++ lib/aws-cdk-pipelines-workflow-typescript-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import {DynamoDbConstruct} from './dynamodb_stack'
+
+const target_environment = 'dev'
+const logical_id_prefix = 'DataLakeTypeScript'
+const resource_name_prefix = 'abc'
 
 export class AwsCdkPipelinesWorkflowTypescriptStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const dynamo_table = new DynamoDbConstruct(this,`${target_environment}${logical_id_prefix}EtlDynamoDb`,{})
   }
 }
---
+++ lib/dynamodb_stack.ts
@@ -0,0 +1,37 @@
+
+import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+
+const target_environment = 'DEV'
+const logical_id_prefix = 'DataLakeTypeScript'
+const resource_name_prefix = 'abc'
+
+
+
+
+
+export interface ConstructorNameProps {
+
+}
+
+export class DynamoDbConstruct extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: ConstructorNameProps) {
+    super(scope, id);
+
+
+    const table = new dynamodb.Table(this,
+      `${target_environment}${logical_id_prefix}EtlAuditTable`, {
+      tableName: `${target_environment.toLowerCase()}-${resource_name_prefix}-etl-job-audit`,
+      partitionKey: { name: 'execution_id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      encryption: dynamodb.TableEncryption.DEFAULT,
+      pointInTimeRecovery: false,
+      readCapacity: 5,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      writeCapacity: 5
+    })
+
+
+
+  }
+}
