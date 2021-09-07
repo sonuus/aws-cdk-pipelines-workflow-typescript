50d095561fa5d660b0f1c538ed75659718a6c78b
Sachin Doshi <sddoshi@amazon.com>
2021-09-07 11:37:58 -0400

    added glue and dynamo construct

--- lib/aws-cdk-pipelines-workflow-typescript-stack.ts
+++ lib/aws-cdk-pipelines-workflow-typescript-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
-import {DynamoDbConstruct} from './dynamodb_stack'
+import { DynamoDbConstruct } from './dynamodb_stack'
+import {GlueConstruct} from './glue_stack'
 
 const target_environment = 'dev'
 const logical_id_prefix = 'DataLakeTypeScript'
@@ -10,6 +11,7 @@ export class AwsCdkPipelinesWorkflowTypescriptStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const dynamo_table = new DynamoDbConstruct(this,`${target_environment}${logical_id_prefix}EtlDynamoDb`,{})
+    const dynamo_table = new DynamoDbConstruct(this, `${target_environment}${logical_id_prefix}EtlDynamoDb`, {})
+    const glue_set_up = new GlueConstruct(this,`${target_environment}${logical_id_prefix}EtlGlue`,{})
   }
 }
